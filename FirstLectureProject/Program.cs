d11460db40b413a519ddfc963daa549495125c5d
Joakim Lignerkrona <jockan@icloud.com>
2022-10-25 09:28:20 +0200

    Add project files.

---
+++ FirstLectureProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstLectureProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
