1cfc999c3c279a96560573771da557833c7c7abb
Joakim <jockan@me.com>
2022-11-11 21:53:43 +0100

    final for the week

---
+++ Delegater01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Delegater01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action[] carAction = new Action[] { Accelerate, Decelerate };
+
+            foreach(Action action in carAction)
+            {
+                action();
+
+            }
+
+
+        }
+        static void Accelerate() => Console.WriteLine("Accelerating");
+        static void Decelerate() => Console.WriteLine("Decelerating");
+
+
+    }
+}
---
+++ Delegater02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Delegater02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, bool> PrintIt = PrintItM;
+
+            PrintItM("hello", true);
+            PrintItM("hello", false);
+        }
+        static void PrintItM(string s, bool b)
+        {
+            if(b)
+            {
+                Console.WriteLine(s.ToUpper());
+                return;
+            }
+            Console.WriteLine(s.ToLower());
+        }
+
+    }
+}
